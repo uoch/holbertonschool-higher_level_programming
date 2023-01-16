9131a1d267a0fd7fe4707e335aa96b0760b6cb46
uoch <chaouch.thameurtc61@gmail.com>
2023-01-16 21:33:54 +0100

    be_nerveuse

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[39:66] +
-      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[106:112] +
-      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[:6])
+print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[39:66].lstrip() +
+      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[106:112] .lstrip() +
+      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[:6].lstrip())
