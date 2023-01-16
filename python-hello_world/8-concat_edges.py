28ce34e7b242091273db9e6c34ec83983adfd913
uoch <chaouch.thameurtc61@gmail.com>
2023-01-16 21:39:37 +0100

    be_nerveuse

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + ' ' + str[107:111] + ' ' + str[:6]
-print(str)
+print(str[39:66]+" "+str[106:111]+" "+str[:6])
