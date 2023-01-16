817a51307221e3083f2a236fd9d3824aa5ce4be9
uoch <chaouch.thameurtc61@gmail.com>
2023-01-16 21:47:59 +0100

    be_nervese

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = ("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[39:66] +\
-       "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[106:112] +\
-	   "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[:6])
+str = ('Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax'[39:66] +\
+       'Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax'[106:112] +\
+	   'Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax'[:6])
 print(str)
