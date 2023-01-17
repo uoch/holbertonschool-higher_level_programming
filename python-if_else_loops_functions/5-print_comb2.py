1eb6d6f2cfa85c04845d2e9e863cf360d72262b1
uoch <chaouch.thameurtc61@gmail.com>
2023-01-17 22:42:15 +0100

    second

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02d}" + (", " if i != 99 else "\n"), end="")
+    print("{}".format(i) + (", " if i != 99 else "\n"), end="")
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(0, 10):
         if (i != j and (i-j) != 0) and (i-j) != 1 and (i-j) != 2 and \
             (i-j) != 3 and (i-j) != 4 and (i-j) != 5 and (i-j) != 6\
                 and (i-j) != 7 and (i-j) != 8 and (i-j) != 9:
-            print(f"{i}{j}" + (", " if (i+j) != 17 else "\n"), end="")
+            print("{}".format(i) + "{}".format(j),
+                  (", " if (i+j) != 17 else "\n"), end="")
