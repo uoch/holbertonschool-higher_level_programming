b3998890acdcd1e843d59b3576319cdeb10286de
uoch <chaouch.thameurtc61@gmail.com>
2023-01-17 20:54:38 +0100

    first commit

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(10):
+        if (i != j and (i-j) != 0) and (i-j) != 1 and (i-j) != 2 and \
+            (i-j) != 3 and (i-j) != 4 and (i-j) != 5 and (i-j) != 6\
+                and (i-j) != 7 and (i-j) != 8 and (i-j) != 9:
+            print(f"{i}{j}" + (", " if (i+j) != 17 else "\n"), end="")
