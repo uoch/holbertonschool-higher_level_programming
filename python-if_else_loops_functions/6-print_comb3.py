170d1dcfae69418a31b1d491e82456cef16aa6d4
uoch <chaouch.thameurtc61@gmail.com>
2023-01-17 22:50:09 +0100

    second

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,4 @@ for i in range(0, 10):
         if (i != j and (i-j) != 0) and (i-j) != 1 and (i-j) != 2 and \
             (i-j) != 3 and (i-j) != 4 and (i-j) != 5 and (i-j) != 6\
                 and (i-j) != 7 and (i-j) != 8 and (i-j) != 9:
-            print("{}".format(i) + "{}".format(j),
-                  (", " if (i+j) != 17 else "\n"), end="")
+            print("{}{}".format(i, j) + (", " if (i+j) != 17 else "\n"), end="")
