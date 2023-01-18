554026193143f44561eda456c5a24b899a6b24b0
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:12:15 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    from calculator_1 import add, div, mul, sub
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    result2 = div(a, b)
+    result3 = mul(a, b)
+    result4 = sub(a, b)
+    print("{} + {} = {}".format(a, b, result1))
+    print("{} - {} = {}".format(a, b, result4))
+    print("{} * {} = {}".format(a, b, result3))
+    print("{} / {} = {}".format(a, b, result2))
