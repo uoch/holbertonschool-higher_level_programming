99d8291caa72bc8409bfaeff70c377958b3efbdb
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:22:45 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, mul, sub
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    from calculator_1 import add, div, mul, sub
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
