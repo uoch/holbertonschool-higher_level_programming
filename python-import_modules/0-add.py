3fea247fb105043eca2e9e072f8078a2a9105b52
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:31:59 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from add_0 import add
 
 if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from  import add
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    from calculator_1 import add, div, mul, sub
+
+if __name__ == "__main__":
+    from calculator_1 import add, div, mul, sub
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    result2 = div(a, b)
+    result3 = mul(a, b)
+    result4 = sub(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result1))
+    print("{:d} - {:d} = {:d}".format(a, b, result4))
+    print("{:d} * {:d} = {:d}".format(a, b, result3))
+    print("{:d} / {:d} = {:d}".format(a, b, result2))
