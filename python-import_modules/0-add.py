ccaca386268dac1ae68f5260b82165e69a346c22
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:03:43 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return a + b
-
 if __name__ == "__main__":
-	__import__('0-add')
+	from add_0 import add
 	a = 1
 	b = 2
 	result = add(a, b)
