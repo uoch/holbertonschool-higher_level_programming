9acec88281b1d68d68ad581644c577e005fd2cb9
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 22:31:33 +0100

    test

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b
+
+
+a = 1
+b = 2
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
