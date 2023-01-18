5e0ccd4927dc52e2930e0831d6ad033a0ced4542
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 20:43:32 +0100

    fix12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,11 +4,11 @@ def fizzbuzz():
         if (i % 15 == 0):
             print("FizzBuzz", end=" ")
 
-        if (i % 5 == 0):
+        elif (i % 5 == 0):
             print("Buzz", end=" ")
 
-        if (i % 3 == 0):
+        elif (i % 3 == 0):
             print("Fizz ", end="")
 
         else:
-            print("{}".format(i), end=" ")
+            print(i, end=" ")
