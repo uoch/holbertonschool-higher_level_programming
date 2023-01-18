f27974fb6d1ddf1190d41eeead5253075bce84fe
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:32:58 +0100

    test

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, div, mul, sub
+from calculator_1 import add, div, mul, sub
 
 if __name__ == "__main__":
-    from calculator_1 import add, div, mul, sub
+
     a = 10
     b = 5
     result1 = add(a, b)
