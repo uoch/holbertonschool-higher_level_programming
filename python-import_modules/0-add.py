31376d9d279219117860e553679518180100567b
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 23:20:59 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, div, mul, sub
+
 if __name__ == "__main__":
     from calculator_1 import add, div, mul, sub
     a = 10
     b = 5
-    result1 = add(a, b)
-    result2 = div(a, b)
-    result3 = mul(a, b)
-    result4 = sub(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result1))
-    print("{:d} - {:d} = {:d}".format(a, b, result4))
-    print("{:d} * {:d} = {:d}".format(a, b, result3))
-    print("{:d} / {:d} = {:d}".format(a, b, result2))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
