5136d9e83dcf480be26ee0168158f49937ad07fe
uoch <chaouch.thameurtc61@gmail.com>
2023-01-18 22:41:21 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -11,8 +11,10 @@ def add(a, b):
         The return value. a + b
     """
     return a + b
+from /home/uoch/holbertonschool-higher_level_programming/python-import_modules/0-add.py import add
 
 if __name__ == "__main__":
+	
 	a = 1
 	b = 2
 	result = add(a, b)
