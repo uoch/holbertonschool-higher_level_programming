7bf4af9c8f2339263306e7d2c0df1758f2328657
uoch <chaouch.thameurtc61@gmail.com>
2023-01-19 16:36:36 +0100

    add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if (k <= 1):
         print("0")
         sys.exit()
-    for i in range(1,k):
-            if sys.argv[i].isnumeric() or (sys.argv[i][1:].isnumeric() and sys.argv[i][0] == '-'):
-                r += int(sys.argv[i])
+    for i in range(1, k):
+        if sys.argv[i].isnumeric() or (sys.argv[i][1:].isnumeric() and sys.argv[i][0] == '-'):
+            r += int(sys.argv[i])
     print(r)
