e42d8b4862fc1db71103bf2163f15e1f52681a35
uoch <chaouch.thameurtc61@gmail.com>
2023-01-19 16:35:23 +0100

    add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    k = len(sys.argv)
+    r = 0
+    if (k <= 1):
+        print("0")
+        sys.exit()
+    for i in range(1,k):
+            if sys.argv[i].isnumeric() or (sys.argv[i][1:].isnumeric() and sys.argv[i][0] == '-'):
+                r += int(sys.argv[i])
+    print(r)
