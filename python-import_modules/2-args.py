cc26cd5d4972c1eb026ea3482eab3276e1d33c0b
uoch <chaouch.thameurtc61@gmail.com>
2023-01-19 15:58:20 +0100

    argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    k = len(sys.argv)
+    if (k <=2):
+        print("{} argument:".format(k-1))
+        print("{}: {}".format(k-1,sys.argv[k-1]))
+    else:
+        for i in range(k):
+            if ( i == 0):
+                print("{} arguments:".format(len(sys.argv)-1))
+            else:
+                print("{}: {}".format(i,sys.argv[i]))
