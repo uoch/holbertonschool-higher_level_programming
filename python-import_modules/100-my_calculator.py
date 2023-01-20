dff7c4b132462303b798e563e8f786b3298694c8
uoch <chaouch.thameurtc61@gmail.com>
2023-01-20 18:17:28 +0100

    100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-
+    operator_found = False
     op = ["+", "-", "/", "*"]
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
