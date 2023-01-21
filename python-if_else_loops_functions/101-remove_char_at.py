f4c12a495c3c2ac1d9ea460ec0ee5fcd0dc710a1
uoch <chaouch.thameurtc61@gmail.com>
2023-01-21 12:42:25 +0100

    101

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     new_str = ""
     for i in range(len(str)):
         if i != n:
-            if (65 <= ord(str[i]) <= 90 or 97
-                    <= ord(str[i]) <= 122) or str[i] == " ":
+            if (65 <= ord(str[i]) <= 90
+                    or 97 <= ord(str[i]) <= 122) or str[i] == " ":
                 new_str += str[i]
     return new_str
