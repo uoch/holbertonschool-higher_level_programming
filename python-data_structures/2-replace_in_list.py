b8720e1b23c80ce80de1d27daa27b5479de5737d
uoch <chaouch.thameurtc61@gmail.com>
2023-01-23 19:51:46 +0100

    2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    if idx <= 0:
         return my_list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
