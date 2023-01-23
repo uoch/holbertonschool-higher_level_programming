f950dc49172a47eb588cbd9613d8a3bd768f6ec9
uoch <chaouch.thameurtc61@gmail.com>
2023-01-23 22:33:34 +0100

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,14 +7,3 @@ def print_matrix_integer(matrix=[[]]):
                 break
             print("{:d} ".format(matrix[i][j]), end='')
         print(end="\n")
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
