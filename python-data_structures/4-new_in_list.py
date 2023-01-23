36c4196042478d0c1724e3251e2e9ed37940a1ba
uoch <chaouch.thameurtc61@gmail.com>
2023-01-23 20:45:59 +0100

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+It is important to note that in python lists are mutable objects, 
+so if you modify the original list, it will change the copy too,
+so it's important to use the .copy() method to create a new list and prevent modifying the original list
+"""
 def new_in_list(my_list, idx, element):
 
     new = my_list.copy()
