eec05f7a0a53fa94ccf55212eac668e11d0fc473
uoch <chaouch.thameurtc61@gmail.com>
2023-01-24 20:47:39 +0100

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ 
-The del statement is used to delete an element, a variable or an object from memory. 
+The del statement is used to delete an element, a variable 
+or an object from memory. 
 It can be used to delete a variable, 
 list element, dictionary item or an item of an object.
 """
 
 
 def delete_at(my_list=[], idx=0):
-    if idx <= 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     del my_list[idx]
     return my_list
