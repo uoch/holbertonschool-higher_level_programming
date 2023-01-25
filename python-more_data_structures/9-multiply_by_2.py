013be84ea1a0433ce17426def36789335bc14d70
uoch <chaouch.thameurtc61@gmail.com>
2023-01-25 22:53:48 +0100

    9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary is none:
+        return None
+    n = a_dictionary.copy
+    for key in n.keys():
+        n[key] *= 2
+    return n
