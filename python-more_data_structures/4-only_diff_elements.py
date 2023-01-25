6c708be40fcd5197a7c231e72b8faefe67024e77
uoch <chaouch.thameurtc61@gmail.com>
2023-01-25 20:04:30 +0100

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    k = []
+    # https://realpython.com/python-sets/
     if (set_1 is None) or (set_2 is None):
         return None
+        """
     for i in set_1:
         for j in set_2:
             if not (i == j):
                 k.append(i)
-    return (k)
-#result = list(set_1.common_elements(set_2))
+        return (k)
+	"""
+        result = list(set_1 ^ set_2)
+        # x1.symmetric_difference(x2)
+        return result
