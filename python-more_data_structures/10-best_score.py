f64e0ca03b32c43a8889c4ff528412e71db23948
uoch <chaouch.thameurtc61@gmail.com>
2023-01-25 23:29:16 +0100

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
@@ -8,3 +9,8 @@ def best_score(a_dictionary):
         for k in a_dictionary.keys():
             if a_dictionary[key] < a_dictionary[k]:
                 return k
+"""
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
