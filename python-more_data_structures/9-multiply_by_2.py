708defd8d5fd32946cc80e82d2b4677f574fdea2
uoch <chaouch.thameurtc61@gmail.com>
2023-01-25 22:54:29 +0100

    9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary is none:
         return None
-    n = a_dictionary.copy
+    n = a_dictionary.copy()
     for key in n.keys():
         n[key] *= 2
     return n
