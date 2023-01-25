71cb87ed12bdf695f79c3cd65c64c551eacb93bc
uoch <chaouch.thameurtc61@gmail.com>
2023-01-25 19:01:06 +0100

    1,2

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    n = my_list.copy()
+    for i in range(len(n)):
+        if n[i] == search:
+            n[i] = replace
+    return n
