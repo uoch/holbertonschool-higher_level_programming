c60b2bd32f7297f23c082f2a90741123c906ace1
uoch <chaouch.thameurtc61@gmail.com>
2023-01-26 22:45:23 +0100

    4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             n.append(0)
         except IndexError:
-            print("Index out of range")
+            print("out of range")
             n.append(0)
         finally:
             pass
