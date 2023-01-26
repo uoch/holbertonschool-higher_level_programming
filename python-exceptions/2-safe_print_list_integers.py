8f36682da0d6edfc9445d5b2d2064382ccfac219
uoch <chaouch.thameurtc61@gmail.com>
2023-01-26 21:24:19 +0100

    2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    k = 0
+    while x > 0:
+        try:
+            if isinstance(my_list[k], int):
+                print("{:d}".format(my_list[k]), end="")
+                i += 1
+            k += 1
+        except IndexError:
+            print(IndexError)
+        x -= 1
+    print()
+    return i
