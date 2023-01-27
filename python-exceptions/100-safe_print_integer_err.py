48a7e3d01b25c625981a8f3a475ff0bfb464e262
uoch <chaouch.thameurtc61@gmail.com>
2023-01-27 16:33:54 +0100

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -8,6 +8,7 @@ def safe_print_integer_err(value):
         return True
     except ValueError as e:
         sys.stderr.write("Exception: {}\n".format(str(e)))
+        return False
     except TypeError as e:
         sys.stderr.write("Exception: {}\n".format(str(e)))
         return False
