633e5816769b6a148ef43208af78b6909a51a0d9
uoch <chaouch.thameurtc61@gmail.com>
2023-01-27 11:00:24 +0100

    102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -5,3 +5,5 @@ def complex_delete(a_dictionary, value):
     for key in sorted(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
+
+    return a_dictionary
