540e5a514b3062064daa0ad191fd33c9f59328d1
uoch <chaouch.thameurtc61@gmail.com>
2023-01-27 16:17:26 +0100

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,10 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except ValueError as e:
         sys.stderr.write("Exception: {}\n".format(str(e)))
+    except TypeError as b:
+        sys.stderr.write("Exception: {}\n".format(str(b)))
         return False
