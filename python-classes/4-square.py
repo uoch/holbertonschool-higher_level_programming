62860991440ff554478a6650c94b752ae4570629
uoch <chaouch.thameurtc61@gmail.com>
2023-01-30 22:10:59 +0100

    property_setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,10 +6,6 @@
 
 class Square:
     """A class with a private attribute `size` to represent 
-    the size of the square, and
-    methods to handle the size value as an integer and positive,
-    here we impliment the propriety and setter for the attrubute setter 
-    and calculate the area of the square.
     """
 
     def __init__(self, size=0):
