4eba070c6593f969ad483cdde61f9592714a0dd0
uoch <chaouch.thameurtc61@gmail.com>
2023-01-30 22:04:42 +0100

    property_setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains the square class with size have been defined.
+    end calculate the area of the square
+    """
+
+
+class Square:
+    """A class with a private attribute `size` to represent the size of the square, and
+    methods to handle the size value as an integer and positive, and calculate the area of the square.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
