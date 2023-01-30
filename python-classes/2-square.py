7465ea33468026a169f395f5ef0026728625cab5
uoch <chaouch.thameurtc61@gmail.com>
2023-01-30 20:43:44 +0100

    size_try

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains the square class with size have been defined."""
+
+
+class Square:
+    """A class with first private attribute with try except to handal size value
+        as integer and positive
+    """
+
+    def __init__(self, size=0):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except (TypeError, ValueError) as e:
+            print(e)
