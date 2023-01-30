2f59dee1763efad0cc2ddd7377a72b790ee1ff62
uoch <chaouch.thameurtc61@gmail.com>
2023-01-30 19:25:49 +0100

    empty

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module documentation.
+
+This module contains the implementation of the EmptyClass class.
+"""
+
+
+class Square:
+    """A class with first private attribute
+    """
+    __size
