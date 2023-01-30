4a4cbe1d6a7c450489c481828333d2d417c21660
uoch <chaouch.thameurtc61@gmail.com>
2023-01-30 19:21:55 +0100

    empty

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Module documentation.
+
+This module contains the implementation of the EmptyClass class.
+"""
+
+
 class Square:
     """A class in Python that does not contain any attributes or methods.
     """
