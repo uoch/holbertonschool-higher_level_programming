38c3f0877d2d2c9be16525cbd65b8a53b9ef30c9
uoch <chaouch.thameurtc61@gmail.com>
2023-01-31 23:53:42 +0100

    test_div

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,13 +9,12 @@ intger or flot
 def matrix_divided(matrix, div):
     """2-matrix_divided.py
     the thing her is all the condition will be printed
-    in the stdout
-    """
+    in the stdout"""
     for r in matrix:
         for el in r:
             if not isinstance(el, (int, float)):
-                raise TypeError("matrix must be a matrix (list of\
-                                              lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
