757dc8a0b7827ce4bd58c66cb447732a96f02edd
uoch <chaouch.thameurtc61@gmail.com>
2023-01-31 22:47:42 +0100

    test_py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """add_int to be tested
     as i undrestand is that the doc.txt
-    works as it is our terminal and we should give the exact result like 
+    works as it is our terminal and we should give the exact result like
     the given from the intrepreter
     python3
     """
@@ -10,7 +10,7 @@
 def add_integer(a, b=98):
     """first we have to check if the given data is a intger
     else we have to raise a typerror
-    to tell the user he must put intger value 
+    to tell the user he must put intger value
     """
     if a != a:
         a = 89
