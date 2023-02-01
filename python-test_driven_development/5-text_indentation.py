76c8aaaee830ace388bdd63d1d046f23fbafd311
uoch <chaouch.thameurtc61@gmail.com>
2023-02-01 20:31:25 +0100

    test_say_my_name

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,8 +9,8 @@ def text_indentation(text):
     for i in text:
         print(i, end="")
         if i == '.':
-            print()
+            print("\n")
         if i == '?':
-            print()
+            print("\n")
         if i == ':':
-            print()
+            print("\n")
