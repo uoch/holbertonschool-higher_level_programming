8d00474327ca67d055f1f73c0c07d11ec6828892
uoch <chaouch.thameurtc61@gmail.com>
2023-02-01 19:57:20 +0100

    test_say_my_name

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """4-print_square.py"""
+
+
 def print_square(size):
     """4-print_square.py"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise TypeError("size must be >= 0")
-    if  isinstance(size, float) and size<0 :
+    if isinstance(size, float) and size < 0:
         raise TabError(("size must be an integer"))
     for i in range(size):
         print(size*'#')
-    print()
