30e82ee3e1bb6b8d65b210f9a6cba3787a56e09b
uoch <chaouch.thameurtc61@gmail.com>
2023-02-01 20:25:26 +0100

    test_say_my_name

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""5-text_indentation.py"""
+
+
+def text_indentation(text):
+    """5-text_indentation.py"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        print(i, end="")
+        if i == '.':
+            print()
+        if i == '?':
+            print()
+        if i == ':':
+            print()
+    print()
