3d8f476aa3f7b365b648cf22beac1fb18e8cbcd4
uoch <chaouch.thameurtc61@gmail.com>
2023-02-01 23:08:43 +0100

    matrix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,6 +12,10 @@ def add_integer(a, b=98):
     else we have to raise a typerror
     to tell the user he must put intger value
     """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
