ac53dbee2a5081d8f680dd6f549976a88d63c99a
uoch <chaouch.thameurtc61@gmail.com>
2023-02-03 13:02:40 +0100

    tst

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,24 +3,13 @@
 
 
 def text_indentation(text):
-    """text_indentation
-
-    Args:
-            text (string): input from user
-
-    Raises:
-            TypeError: text must be a string
-    """
+    """5-text_indentation.py"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    i = 0
-    while i < len(text):
-        if text[i] in [".", ":", "?"]:
-            print(text[i])
-            print()
-            i += 1
-        while i < len(text) and text[i] == " ":
-            i += 1
-        else:
-            print(text[i], end="")
-        i += 1
+    for i in " ".join(text.split()):
+        print(i, end="")
+        if i in (':', '.', '?'):
+            print("\n", end="\n")
+
+
+text_indentation("Holberton. School? How are you:    John")
