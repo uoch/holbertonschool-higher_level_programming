db54f1c841bbd50de8b0c8ef348eda223a73a2a5
uoch <chaouch.thameurtc61@gmail.com>
2023-02-03 12:52:31 +0100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,11 @@
 
 
 def text_indentation(text):
-    if not isinstance(text,str):
-        raise TypeError ("text must be a string")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     i = 0
     while i < len(text):
-        if text[i] in [".",":","?"]:
+        if text[i] in [".", ":", "?"]:
             print(text[i])
             print()
             i += 1
