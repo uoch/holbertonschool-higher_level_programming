65a9edd15a2702323c69ae973f38f30fcb522d7c
uoch <chaouch.thameurtc61@gmail.com>
2023-02-03 16:20:53 +0100

    100-singly_linked_list.py

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -40,8 +40,23 @@ class SinglyLinkedList:
     def __init__(self):
         self.__head = None
 
-    def print_list(self):
-        if node.next_node == __head:
-            while Node:
-            	print(Node.data)
-            	node = Node.next_node
+    def sorted_insert(self, value):
+        new = Node(value, self.__head)
+        c = self.__head
+        p = None
+        while c and c.data < value:
+            p = c
+            c = c.next_node
+        if p:
+            p = new
+        else:
+            self.__head = new
+        new.next_node = c
+
+    def __str__(self):
+        r = ""
+        node = self.__head
+        while node:
+            r += str(node.data) + "\n"
+            node = node.next_node
+        return r
