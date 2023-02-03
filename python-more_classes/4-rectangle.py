c456339ee50c4174e200819d46e54d3ba25b3ee6
uoch <chaouch.thameurtc61@gmail.com>
2023-02-03 08:53:18 +0100

    4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         return self.my_print()
 
     def __repr__(self):
-        return f"Rectangle({self.height}, {self.width})"
+        return f"Rectangle({self.width}, {self.height})"
 
     def area(self):
         return (self.__height*self.__width)
