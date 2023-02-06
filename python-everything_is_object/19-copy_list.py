d8e309b44ad84f876236b8349f405354cb5f6c8f
uoch <chaouch.thameurtc61@gmail.com>
2023-02-06 19:38:44 +0100

    0-answer.txt

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""19-copy_list.py"""
+
+
+def copy_list(a_list):
+    """_summary_
+
+    Args:
+        a_list (any type): input
+
+    Returns:
+        _type_: a copy of object
+    """
+    return a_list.copy()
