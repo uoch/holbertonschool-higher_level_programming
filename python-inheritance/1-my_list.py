4d9e18191eb181e5beb52d03a618be3368e0ca57
uoch <chaouch.thameurtc61@gmail.com>
2023-02-06 22:45:52 +0100

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -18,13 +18,3 @@ class MyList(list):
 
     def __str__(self):
         return str(list(self))
-
-
-my_list = MyList()
-print(my_list)
-my_list.append(3)
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.print_sorted()
-print(my_list)
