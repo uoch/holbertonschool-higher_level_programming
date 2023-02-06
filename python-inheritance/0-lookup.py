b73bad45578b282c347d87ee6682c4e8b3fde1d6
uoch <chaouch.thameurtc61@gmail.com>
2023-02-06 21:40:40 +0100

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""_summary_
+    """
+
+
+def lookup(obj):
+    """_summary_
+
+    Args:
+        obj (any object): dir built-in function to retrieve a list
+        of the attributes of the object. 
+
+    Returns:
+        _type_: list
+    """
+    return dir(obj)
