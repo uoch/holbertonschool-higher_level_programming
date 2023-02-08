cc714d970a22e40c25675155d091c971cd7c3bc1
uoch <chaouch.thameurtc61@gmail.com>
2023-02-08 21:33:45 +0100

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""
+a class Square that inherits from Rectangle (9-rectangle.py):
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    *Instantiation with size: def __init__(self, size)::
+    *size must be private. No getter or setter
+    *size must be a positive integer, validated by integer_validator
+    *the area() method must be implemented
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return super().__str__()
