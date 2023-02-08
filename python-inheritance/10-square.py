a616df440e72e14a9752fce07ddbe1219a6dbf9d
uoch <chaouch.thameurtc61@gmail.com>
2023-02-08 20:58:18 +0100

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,7 +5,39 @@ his task is for implimenting squar
 from Rectangle
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
+class BaseGeometry:
+    """area() is not implemented
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle (BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (is a class): that created in module 7-base_geometry
+        with the function integer_validator
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
