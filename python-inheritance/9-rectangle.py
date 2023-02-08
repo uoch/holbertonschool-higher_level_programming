8b7e11a6de85652d8f48c647516c376a7c3664e3
uoch <chaouch.thameurtc61@gmail.com>
2023-02-08 20:31:55 +0100

    9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""8-rectangle.py"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle (BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (is a class): that created in module 7-base_geometry
+        with the function integer_validator
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
