8d8f52335adeea6b974af51a59315f633280a567
uoch <chaouch.thameurtc61@gmail.com>
2023-02-08 20:46:53 +0100

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
-"""t
-his task is for implimenting squar 
-from Rectangle
+"""
+a class Square that inherits from Rectangle (9-rectangle.py):
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
+class Square(Rectangle):
+    """
+    *Instantiation with size: def __init__(self, size)::
+    *size must be private. No getter or setter
+    *size must be a positive integer, validated by integer_validator
+    *the area() method must be implemented
+    """
 
-class Square (Rectangle):
-	"""Square
-
-	Args:
-		Rectangle (mother_class):
-	"""
-	def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
-
-	def area(self):
-        return self.__size*self.__size
-
-	def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+    def __init__(self, size):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        super().__init__(size, size)
