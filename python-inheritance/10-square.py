22d2b57153394e45295fe7d6002bb0d60e40f97e
uoch <chaouch.thameurtc61@gmail.com>
2023-02-08 21:00:47 +0100

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -18,5 +18,5 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-    	self.integer_validator("size", size)
-    	super().__init__(size, size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
