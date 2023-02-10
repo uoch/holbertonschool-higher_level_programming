2ad1329db15a3a59ffb51cd4bb7432b23b5bfd17
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 15:21:04 +0100

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""6-load_from_json_file.py"""
+import json
+def load_from_json_file(filename):
+	"""load_from_json_file
+
+	Args:
+		filename (_type_): json_file
+
+	Returns:
+		_type_: python objects
+	"""
+	with open(filename,'r',encoding="utf-8")as f:
+        return json.load(f)
