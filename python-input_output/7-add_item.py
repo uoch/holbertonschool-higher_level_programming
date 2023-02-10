1d9ba328195f22ac30ac82f2b6671cadbdbadf00
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 23:01:21 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """7-add_item.py"""
+
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -13,7 +14,7 @@ if __name__ == "__main__":
         with open("add_item.json", "r") as file:
             lines = file.readlines()
         with open("add_item.json", "w") as file:
-        	for line in lines[1:]:
+            for line in lines[1:]:
             	file.write(line)
         items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
