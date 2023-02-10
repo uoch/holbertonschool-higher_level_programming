815223266962256a2f09ddaea97477c5daaafcc3
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 11:02:43 +0100

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""3-to_json_string.py"""
+import json
+
+
+def to_json_string(my_obj):
+    """to_json_string
+
+    Args:
+        my_obj (_type_): the data to be serialized
+
+    Returns:
+        _type_: str(obj)
+    """
+    with open('f.json', 'w') as fp:
+        json.dump(my_obj, fp)
+    return str(my_obj)
