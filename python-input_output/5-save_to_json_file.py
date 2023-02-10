1fcc6df45a0fa9ffae6b6aaa216f8dd604d476a9
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 22:53:05 +0100

    fix_5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,9 +9,7 @@ def save_to_json_file(my_obj, filename):
         my_obj (_type_): object to be written in json string
         filename (_type_): the file to store it
     """
-    if type(my_obj) == set:
-        k = str(list(my_obj))
-    else:
-        k = json.dumps(my_obj)
-    with open(filename, 'a', encoding="utf-8") as f:
-        f.write(k)
+    def save_to_json_file(items, filename):
+        with open(filename, "w") as f:
+            json.dump(items, f)
+
