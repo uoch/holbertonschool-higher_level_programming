9fe243b143348cabc98e8ba4b826ab65c1f8ef43
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 17:24:12 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,9 +6,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if __name__ == "__main__":
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.append(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    if len(sys.argv)==1:
+        try:
+            items = load_from_json_file("add_item.json")
+        except FileNotFoundError:
+            items = []
+    else:
+        items.append(sys.argv[1:])
+        save_to_json_file(items, "add_item.json")
