8766701d23051f636703c2286638deab294e4672
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 13:24:32 +0100

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""5-save_to_json_file.py"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file
+
+    Args:
+        my_obj (_type_): object to be written in json string 
+        filename (_type_): the file to store it
+    """
+    k = json.dumps(my_obj)
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(k)
