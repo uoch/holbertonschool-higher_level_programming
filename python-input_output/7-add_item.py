4f7c922de91eda2292e0d38f036ea41c4442a7de
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 22:59:48 +0100

    fix_5

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""7-add_item.py"""
+"""7-add_item
+.py"""
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
