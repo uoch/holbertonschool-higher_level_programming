af13e740c6042456d1ec1f1cabed9c722bad90f5
uoch <chaouch.thameurtc61@gmail.com>
2023-02-10 16:48:09 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,4 +6,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
 
-    save_to_json_file(load_from_json_file("add_item.json", "add_item.json"))
+    items = load_from_json_file("add_item.json")
+    items.append(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
