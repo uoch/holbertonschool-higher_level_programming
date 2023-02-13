cacc1c72eaa89e0d68bb65e4a9e1d3859ef6084e
uoch <chaouch.thameurtc61@gmail.com>
2023-02-13 21:45:52 +0100

    rectang

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """rectangle.py
-is a class inherteds 
+is a class inherteds
 from Base"""
 from models.base import Base
 
@@ -12,14 +12,14 @@ class Rectangle (Base):
         Base (_type_): _description_
     """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+	def __init__(self, width, height, x=0, y=0, id=None):
+		super().__init__(id)
+		self.__width = width
+		self.__height = height
+		self.__x = x
+		self.__y = y
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-
-    def strictly_greater_than_zero(self, string, value):
+	def strictly_greater_than_zero(self, string, value):
         """strictly_greater_than_zero
 
         Args:
@@ -31,7 +31,7 @@ class Rectangle (Base):
         if value <= 0:
             raise ValueError("{} must be > 0".format(string))
 
-    def greater_than_zero(self, string, value):
+	def greater_than_zero(self, string, value):
         """greater_than_zero
 
         Args:
@@ -43,38 +43,38 @@ class Rectangle (Base):
         if value < 0:
             raise ValueError("{} must be > 0".format(string))
 
-    @property
-    def height(self):
+	@property
+	def height(self):
         return self.__height
 
-    @height.setter
-    def height(self, value):
+	@height.setter
+	def height(self, value):
         self.strictly_greater_than_zero("height", value)
         self.__height = value
 
-    @property
-    def width(self):
+	@property
+	def width(self):
         return (self.__width)
 
-    @width.setter
-    def width(self, value):
+	@width.setter
+	def width(self, value):
         self.strictly_greater_than_zero("width", value)
         self.__width = value
 
-    @property
-    def x(self):
+	@property
+	def x(self):
         return self.__x
 
-    @x.setter
-    def x(self, value):
+	@x.setter
+	def x(self, value):
         self.greater_than_zero("x", value)
         self.__x = value
 
-    @property
-    def y(self):
+	@property
+	def y(self):
         return self.__y
 
-    @y.setter
-    def y(self, value):
+	@y.setter
+	def y(self, value):
         self.greater_than_zero("y", value)
         self.__y = value
