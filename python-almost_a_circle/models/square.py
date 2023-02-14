ed720fd6ce758e7867032835baf1f54a800473e4
uoch <chaouch.thameurtc61@gmail.com>
2023-02-14 22:59:12 +0100

    rec_dic_presantation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -55,6 +55,11 @@ class Square (Rectangle):
                 super(Rectangle, self).__init__(kwargs['id'])
 
     def to_dictionary(self):
+        """to_dictionary
+
+        Returns:
+            dic: dictionnary presntation
+        """
         return {'id': self.id,
                 'x': self.x,
                 'size': self.size,
