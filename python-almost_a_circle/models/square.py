c7e00282d9e777681a80508ce2da6df7d1154adb
uoch <chaouch.thameurtc61@gmail.com>
2023-02-14 21:58:08 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,4 +19,5 @@ class Square (Rectangle):
         self.size = size
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))
