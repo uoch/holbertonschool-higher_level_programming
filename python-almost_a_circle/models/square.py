6dad86700d64c01da6164f5e223ac27b31917653
uoch <chaouch.thameurtc61@gmail.com>
2023-02-14 21:54:27 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,7 +14,8 @@ class Square (Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
