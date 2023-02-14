f13c247a76b6e13a01d91cbcf4338db5d4f43e2f
uoch <chaouch.thameurtc61@gmail.com>
2023-02-14 21:37:40 +0100

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""rectangle.py
+is a class inherteds
+from Base"""
+
+from models.base import Base, Rectangle
+
+
+class Square (Rectangle):
+    """Square
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
