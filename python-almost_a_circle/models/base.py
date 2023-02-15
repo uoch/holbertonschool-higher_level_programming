508c12f2caff4e3e1634a9b5beff2f7fc4ed69ab
uoch <chaouch.thameurtc61@gmail.com>
2023-02-15 22:29:01 +0100

    to-dic

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -48,3 +48,15 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+    
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            return None
+        dummy.update(**dictionary)
+        return dummy
