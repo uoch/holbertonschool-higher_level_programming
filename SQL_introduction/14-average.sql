84e9c2facc2b4d6cfe97a30d475ddee803c4259f
uoch <chaouch.thameurtc61@gmail.com>
2023-03-14 20:58:19 +0100

    test

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT AVG (score) FROM second_table;
+SELECT AVG (score) AS average FROM second_table;
