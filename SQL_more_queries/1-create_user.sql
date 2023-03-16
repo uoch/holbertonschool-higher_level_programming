50db70d3f49302c16cd72f91487e32143640011a
uoch <chaouch.thameurtc61@gmail.com>
2023-03-16 20:20:07 +0100

    1er

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates the MySQL server user user_0d_1
+-- The CREATE USER statement creates a new user with the username 'user_0d_1' and the password 'user_0d_1_pwd'. The IF NOT EXISTS clause checks if the user already exists, and if it does, the statement does nothing.
+-- The GRANT ALL PRIVILEGES statement grants all privileges on all databases and tables to the user 'user_0d_1'@'localhost'. This allows the user to perform any action on the MySQL server.
+-- Note that you may need to modify the script depending on your MySQL server configuration and the hostname or IP address of the client connecting to the server.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
--- Explanation:
 -- The 'CREATE' DATABASE statement creates a new database called hbtn_0d_2. The IF NOT EXISTS clause checks if the database already exists, and if it does, the statement does nothing.
 -- The 'CREATE' USER statement creates a new user with the username 'user_0d_2' and the password 'user_0d_2_pwd'. The IF NOT EXISTS clause checks if the user already exists, and if it does, the statement does nothing.
 -- The 'GRANT' SELECT statement grants the SELECT privilege on all tables in the database hbtn_0d_2 to the user 'user_0d_2'@'localhost'. This allows the user to only retrieve data from the database and not modify it.
