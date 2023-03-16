5b8eb8f0ea81c75d6fc2a61b8cc99868c459832d
uoch <chaouch.thameurtc61@gmail.com>
2023-03-16 20:51:27 +0100

    1er

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- reates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa ;
 use hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS states IN hbtn_0d_usa (id INT NOT NULL AUTO_INCREMENT ,  name VARCHAR(256),UNIQUE (id), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT ,  name VARCHAR(256),UNIQUE (id), PRIMARY KEY (id));
