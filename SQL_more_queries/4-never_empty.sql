9ecd0fff48cafb093d5a486eca78a56f28963154
uoch <chaouch.thameurtc61@gmail.com>
2023-03-16 20:23:42 +0100

    1er

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,1 +1,2 @@
+-- The DEFAULT keyword specifies a default value for the "id" column. In this case, the default value is 1.
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1,  name VARCHAR(256) NOT NULL);
