9fe04ca72c5d0a690a07c125bce60485112b4f5d
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 17:17:56 +0100

    pysql

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Retrieve command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=db_name)
+
+    # Create cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve all cities from database and sort by id
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+
+    # Fetch all rows returned by query
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
