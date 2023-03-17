9acc484f61d4ccf2da5d6b984eae7fe4cc2203ee
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 08:29:04 +0100

    2nd

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- cript that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title , tv_show_genres.genre_id FROM tvshows
-INNER JOIN tv_shows.title = tv_show_genres.genre_id
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY tv_shows.titlec ASC , tv_show_genres.genre_id  ASC;
