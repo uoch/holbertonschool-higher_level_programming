2e62e6e972656f8a5cbe0a4675ab54ae3bf71705
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 10:02:18 +0100

    2nd

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- tained in hbtn_0d_without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_show FULL OUTER JOIN tv_show_genres
+FROM tv_show LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.id
+WHERE tv_show_genres.genre_id = NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
