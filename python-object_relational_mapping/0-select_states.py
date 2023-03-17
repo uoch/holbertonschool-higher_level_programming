ff45154464cb608ba2e4ff9ff327fc5ae5257545
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 16:25:47 +0100

    pysql

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-	"""cript that lists all states from the database hbtn_0e_0_usa
-	"""
+"""cript that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
