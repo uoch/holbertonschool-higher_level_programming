0e4d126af3609d41f1c1b01e7140977abc5f2e5f
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 10:03:15 +0100

    2nd

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- tained in hbtn_0d_without a genre linked.
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server:
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_show LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.id
-WHERE tv_show_genres.genre_id = NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
