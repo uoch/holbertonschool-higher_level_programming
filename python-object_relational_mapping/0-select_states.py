c5cc267d5e175518c24ffbb7ffecda2ca900e8d0
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 16:23:50 +0100

    pysql

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Retrieve command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                        passwd=mysql_password, db=db_name)
+
+    # Create cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve all states from database
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch all rows returned by query
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
