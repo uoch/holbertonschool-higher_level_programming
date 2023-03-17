866622152365e8aa80f60fb06e5a4efa150f6cce
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 17:11:53 +0100

    pysql

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Script that displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Retrieve command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=db_name)
+
+    # Create cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    """ SQL query to retrieve all states where
+    name matches the argument """
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
+        state_name)
+    cursor.execute(query)
+
+    # Fetch all rows returned by query
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
