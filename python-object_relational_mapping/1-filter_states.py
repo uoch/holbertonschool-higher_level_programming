628a8f6bab2e7023410eb4f300bd3f801c7045bc
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 16:40:43 +0100

    pysql

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """cript that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
@@ -25,7 +24,7 @@ if __name__ == '__main__':
 
     # Display results
     for row in rows:
-        if row [1][0] == 'N':
+        if row[1][0] == 'N':
             print(row)
 
     # Close cursor and database connection
