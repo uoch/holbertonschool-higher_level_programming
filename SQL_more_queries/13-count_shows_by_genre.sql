d90cc1340abfd2c514ae6eca028c4fbc162a43d6
uoch <chaouch.thameurtc61@gmail.com>
2023-03-17 10:19:56 +0100

    2nd

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name AS genre , COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY   genre
 ORDER BY number_of_shows DESC;
