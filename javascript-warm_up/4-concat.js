da2157a5a7aa2ccc42acee07f2e81a0808cf294a
uoch <chaouch.thameurtc61@gmail.com>
2023-04-26 17:29:28 +0100

    conc

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,11 +3,9 @@ const is = ' is ';
 const sp = ' '
 const args = process.argv;
 if (args.length === 1) {
-  console.log(args.length);
   console.log("undefined" + is + "undefined");
 } else if (args.length === 4) {
-  console.log(args.length);
-  console.log(args[2] + sp+ args[3]);
+  console.log(args[2] + sp + args[3]);
 } else {
   console.log(args.length);
   console.log(args[2] + is + args[4]);
