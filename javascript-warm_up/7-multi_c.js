18df96b259902b825f4111dbadc239a2f40a8db1
uoch <chaouch.thameurtc61@gmail.com>
2023-04-26 18:07:12 +0100

    loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,6 +2,9 @@
 const args = process.argv;
 const line = 'C is fun';
 const numTimes = parseInt(args[2]);
-for (let i = 0; i < parseInt.numTimes; i++) {
+if (numTimes === undefined) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < numTimes; i++) {
   console.log(line);
 }
