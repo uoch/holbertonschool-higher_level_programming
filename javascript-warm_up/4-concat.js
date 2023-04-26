c0c53faa2c3abdc8b04cb3216568ee433c4bad4a
uoch <chaouch.thameurtc61@gmail.com>
2023-04-26 17:31:38 +0100

    conc

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const is = ' is ';
-const sp = ' '
 const args = process.argv;
 if (args.length === 1) {
-  console.log("undefined" + is + "undefined");
+  console.log('undefined + is + "undefined');
 } else if (args.length === 4) {
-  console.log(args[2] + sp + args[3]);
+  console.log(args[2] + is + args[3]);
 } else {
-  console.log(args.length);
   console.log(args[2] + is + args[4]);
 }
