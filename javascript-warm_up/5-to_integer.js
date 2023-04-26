639efe2318c4e8f1efc2f68515b92544dd5fcb0c
uoch <chaouch.thameurtc61@gmail.com>
2023-04-26 17:47:12 +0100

    conc

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const num = parseInt(process.argv[2]);
 
 if (!isNaN(num)) {
-	console.log(`My number: ${num}`);
+  console.log(`My number: ${num}`);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
