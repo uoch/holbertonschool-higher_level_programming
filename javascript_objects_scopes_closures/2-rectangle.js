41183fe762e2f23a6e4e7e593baa9f2c1dcbf26a
uoch <chaouch.thameurtc61@gmail.com>
2023-04-27 08:56:09 +0100

    rec2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
