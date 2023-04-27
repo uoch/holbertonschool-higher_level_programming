3a031c88b5b6d0a6bef841c8705c9440b0e0298b
uoch <chaouch.thameurtc61@gmail.com>
2023-04-27 09:00:13 +0100

    rec2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !w || !h) {
-      //undifined
+      // undifined
     } else {
       this.width = w;
       this.height = h;
