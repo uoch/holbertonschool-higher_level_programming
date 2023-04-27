560acaeb3558b98459b3f5f5e74a141f93f91018
uoch <chaouch.thameurtc61@gmail.com>
2023-04-27 07:33:36 +0100

    second

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 const a = process.argv;
-let z = 0;
-  a.sort(function(a, b) {
-    return b - a; // ascending order
-  });
-if(a.length===2){
+const z = 0;
+a.sort(function (a, b) {
+  return b - a; // ascending order
+});
+if (a.length === 2) {
   console.log(z);
-}else if(a.length ===3){
+} else if (a.length === 3) {
+  if (a[2] < 0) {
+    console.log(z);
+  }
   console.log(a[2]);
-}else{
+} else {
   console.log(a[3]);
 }
