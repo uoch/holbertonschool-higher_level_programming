2e960f60afd164a4e5ee279752bb255df8c89669
uoch <chaouch.thameurtc61@gmail.com>
2023-04-27 10:12:36 +0100

    8-esrever.js

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function esrever(list){
-    let a = [];
-    let k = list.length -1;
-    for (let i = 0;i < list.length; i++){
-        a[i]= list[k];
-        k-=1;
-    }
-    return a
+function esrever (list) {
+  const a = [];
+  let k = list.length - 1;
+  for (let i = 0; i < list.length; i++) {
+    a[i] = list[k];
+    k -= 1;
+  }
+  return a;
 }
 exports.esrever = esrever;
