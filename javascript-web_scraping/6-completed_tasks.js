4f538fb0c00face1dce57e5bf6b3f63d60fc3742
uoch <chaouch.thameurtc61@gmail.com>
2023-04-27 18:09:46 +0100

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,23 @@
 #!/usr/bin/node
 const movie = process.argv[2];
-const file = process.argv[3];
 const request = require('request');
-let teskComplite = 0;
 
 request(movie, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        let data = JSON.parse(body);
-        let tasksCompleted = data.reduce((acc, task) => {
-            if (task.completed) {
-                if (acc[task.userId]) {
-                    acc[task.userId]++;
-                } else {
-                    acc[task.userId] = 1;
-                }
-            }
-            return acc;
-        }, {});
-        
-        console.log(tasksCompleted);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const tasksCompleted = data.reduce((acc, task) => {
+      if (task.completed) {
+        if (acc[task.userId]) {
+          acc[task.userId]++;
+        } else {
+          acc[task.userId] = 1;
+        }
+      }
+      return acc;
+    }, {});
+
+    console.log(tasksCompleted);
+  }
 });
