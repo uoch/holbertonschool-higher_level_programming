4a7444f86c0d59ed9aa5ab0e0ca112ba5d6ad1eb
uoch <chaouch.thameurtc61@gmail.com>
2023-05-31 11:47:13 +0100

    header

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Display the size of the response body
-curl -s -L "$1"
+response_headers=$(curl -s -I "$1")
+content_length=$(echo "$response_headers" | awk '/Content-Length/ {print $2}' | tr -d '\r')
+echo $content_length 
