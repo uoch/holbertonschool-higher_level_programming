817ec76f76d065e6e1993333fe778ec5bad96e45
uoch <chaouch.thameurtc61@gmail.com>
2023-05-31 15:27:11 +0100

    -I

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the size of the response body
-curl -s -v "$1" --stderr - | grep Content-Length: | cut -d' '  -f 3
+curl -s -I "$1" --stderr - | grep Content-Length: | cut -d' '  -f 3
