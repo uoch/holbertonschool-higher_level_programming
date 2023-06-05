356aec456e1619b1066b7b3af77e858c110774c3
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 09:47:46 +0100

    POST

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     data = data.encode('ascii')  # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        the_page = response.read()
+        print(response.read())
