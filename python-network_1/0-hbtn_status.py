745c28d1592ee5961c7b49570327e0f125ea1e50
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 09:13:52 +0100

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
-"""0. What's my status? #0
-"""
+
 url = 'https://intranet.hbtn.io/status'
+
 with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    body = response.read()
 
-
-print("- Body response:")
+print("Body response:")
 print("\t- type: {}".format(type(body)))
-print("\t- content: b'{}'".format(body))
-print("\t- utf8 content:", body)
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
