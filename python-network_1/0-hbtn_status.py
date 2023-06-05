a842ee89b0136f1e2202cf2a49628fddd1fced05
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 08:55:07 +0100

    status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+import urllib.request
+"""0. What's my status? #0
+"""
+url = 'https://intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("- Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
