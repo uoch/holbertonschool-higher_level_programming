f8a59e6de2a4286487e51d5d65195173678e40ff
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 10:15:12 +0100

    erreur

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""What's my status? #1"""
-import urllib.request
-url = 'https://intranet.hbtn.io/status'
+"""Status of api."""
+import requests
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+r = requests.get('https://intranet.hbtn.io/status')
+content = r.text
 print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body.decode('utf-8'))
+print("\t- type:", type(content))
+print("\t- content:", content)
