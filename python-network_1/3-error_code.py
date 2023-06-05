ff90e687dcd07b0fbdd8c0c8ba304ff3233e0229
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 10:10:01 +0100

    erreur

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """POST an email #0."""
 import urllib.request
-import urllib.error
+from urllib.error import HTTPError
 from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(err.code))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
