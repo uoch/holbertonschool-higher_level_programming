41d21bfb2a11ab7ca1358c09cba709852fc92dc0
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 10:01:33 +0100

    erreur

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = argv[1]
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error code:{}'.format(e.code()))
+        print('Error code:{}'.format(response.status()))
