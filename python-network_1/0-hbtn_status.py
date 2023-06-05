248c2bc6b6a92b861fe5bab0d98b975d6a54677f
uoch <chaouch.thameurtc61@gmail.com>
2023-06-05 09:02:58 +0100

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,4 +8,5 @@ with urllib.request.urlopen(url) as response:
 
 print("- Body response:")
 print("\t- type:", type(body))
-print("\t- content:", body)
+print("\t- content: b", body)
+print("\t- utf8 content", body)
