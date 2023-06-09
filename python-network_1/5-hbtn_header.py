b13051c72bab024753f6a99553c07133950c5cf2
uoch <chaouch.thameurtc61@gmail.com>
2023-06-09 10:27:31 +0100

    5

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+"""Status of api."""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get("X-Request-Id"))
