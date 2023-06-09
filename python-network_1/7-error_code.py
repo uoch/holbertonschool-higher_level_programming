7a78d64254a3996cb2ca88ab22fa351655cc9fc5
uoch <chaouch.thameurtc61@gmail.com>
2023-06-09 10:35:32 +0100

    7

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+"""Handle error codes."""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
+
